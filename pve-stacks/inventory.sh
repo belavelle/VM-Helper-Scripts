85c588213d55972b010cb18de05ec5aca3ade662
belavelle <162368112+belavelle@users.noreply.github.com>
2026-01-07 19:18:58 -0500

    Change PVE_DNS to new IP address

--- pve-stacks/inventory.sh
+++ pve-stacks/inventory.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 PVE_STORAGE="local-lvm"
 PVE_BRIDGE="vmbr0"
-PVE_DNS="1.1.1.1"
+PVE_DNS="192.168.0.1"
 PVE_GATEWAY=""
 PVE_OSTEMPLATE="local:vztmpl/debian-12-standard_12.2-1_amd64.tar.zst"
 
